321dff0cbd243ff79d562683ec492caca0756a2b
ebuc99 <ebuc99@googlemail.com>
2016-01-11 12:44:41 +0100

    version bump

---
+++ src/pacman.cpp
@@ -0,0 +1,36 @@
+#include "pacman.h"
+
+int main(int argc, char *argv[]) {
+	srand((unsigned int)time(0)); // init randomize
+
+	CommandLineOptions::set(argc, argv);
+	if (CommandLineOptions::exists("h", "help")) {
+		std::cout << "This game is a Pacman clone."                             << std::endl
+		          << "Usage: pacman [options]"                                  << std::endl
+		          << std::endl
+		          << "Options:"                                                 << std::endl
+		          << "  -h, --help         Display this help message and quit." << std::endl
+		          << "  -f, --fullscreen   Start the game in fullscreen mode."  << std::endl
+		          << "  -s, --nosound      Start with sound switched off."      << std::endl
+		          << std::endl;
+		return EXIT_SUCCESS;
+	}
+
+	if(Screen::getInstance()->hasSDLInitErrorOccured())
+		return EXIT_FAILURE;
+
+	while(MenuMain::getInstance()->show()) {
+		Game::getInstance()->start();
+	}
+
+	Game::cleanUpInstance();
+	MenuMain::cleanUpInstance();
+	Labyrinth::cleanUpInstance();
+	Pacman::cleanUpInstance();
+	Ghost::cleanUpGhostArray();
+	Sounds::cleanUpInstance();
+	Screen::cleanUpInstance();
+	CommandLineOptions::cleanUp();
+
+	return EXIT_SUCCESS;
+}
